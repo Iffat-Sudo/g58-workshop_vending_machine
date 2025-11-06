86322478c00f66018dc68e432d2b9b19d5008a7f
Iffat-Sudo <zabin.ifa@gmail.com>
2025-11-06 15:08:40 +0100

    Added all the changes

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -1,10 +1,35 @@
 package se.lexicon;
 
 
-public class App 
+import se.lexicon.model.*;
+
+import java.util.Arrays;
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        Product soda = new Soda(30,"Fanta");
+        Product chocolate = new Chocolate( 20,"Kitkat");
+        Product chips = new Chips(25,"Crackers");
+
+        Product[] products = new Product[] {soda,chocolate,chips};
+        VendingMachineImpl vendingMachine = new VendingMachineImpl(products);
+
+        System.out.println(Arrays.toString(vendingMachine.getProducts()));
+
+        System.out.println(vendingMachine.getDescription(0));
+
+        vendingMachine.addCurrency(Currency.FIVE);
+        vendingMachine.addCurrency(Currency.ONE);
+        vendingMachine.addCurrency(Currency.TWO);
+        vendingMachine.addCurrency(Currency.TWO);
+        vendingMachine.addCurrency(Currency.TEN);
+
+        System.out.println(vendingMachine.getBalance());
+
+        int change = vendingMachine.endSession();
+        System.out.println("change:"+change);
+
+        System.out.println(vendingMachine.getBalance());
     }
 }
